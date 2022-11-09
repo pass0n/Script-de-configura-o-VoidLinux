0aed23a34bf87c7a0423a148a443cf2d4e1e65f6
Kayllon S. Passon <114824841+pass0n@users.noreply.github.com>
2022-11-09 00:23:39 -0300

    bspwm

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#script de instalação do meu setup voidlinux.
+#entar no modo super usuário
+su root
+#descriptografar e montar os volumes lvm
+cryptsetup luksOpen /dev/sda2 lvm
+vgchange -ay
+#formatar as partições
+wipefs -a /dev/sda1
+wipefs -a /dev/lvm/root
+wipefs -a /dev/lvm/swap
+#criar as partições
+mkfs.vfat -F32 /dev/sda1
+mkfs.ext4 -L VoidLinux /dev/lvm/root
+mkswap /dev/lvm/swap
+#montar as partições
+mount /dev/lvm/root /mnt
+for dir in dev proc sys run; do mkdir -p /mnt/$dir ; mount --rbind /$dir /mnt/$dir ; mount --make-rslave /mnt/$dir ; done
+mkdir -p /mnt/home
+mount /dev/lvm/home /mnt/home
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+#copiar as chaves do xbps
+mkdir -p /mnt/var/db/xbps/keys
+cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
+#instalar o sistema base e afins.
+xbps-install -Sy -R https://repo-default.voidlinux.org/current -r /mnt base-system base-devel lvm2 cryptsetup grub-x86_64-efi vim
+#remover arquivos e instalar os pré-configurados.
+cd .config
+rm /mnt/etc/crypttab
+rm /mnt/etc/fstab
+rm /mnt/etc/hostname
+rm /mnt/etc/locale.conf
+cp cryptab fstab hostname locale.conf /mnt/etc/
+cp 10-key.conf /etc/dracut.conf.d/
+cp grub /etc/default/
+cd /
+cp /home/void.bk/key /
+#configurações da raiz e root
+chroot /mnt
+chown root:root /
+chmod 755 /
+passwd root
+#instalando o boot
+grub-install --target="x86_64" --efi-directory=:"/boot/efi" --bootloarder-id="VoidLinux" /dev/sda
+#reconfigurar tudo
+xbps-reconfigure -fa
+#sair e reiniciar
+exit
+umount -R /mnt
+reboot
---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Script de configuração do meu setup VoidLinux
+
+#Baixar e atualizar os repositórios void
+sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+sudo xbps-install -Sy
+#criar a pasta para instalação do nvidia DKMS
+sudo mkdir -p /var/lib/dkms
+#baixar os meus pacotes padrões
+sudo xbps-install -Sy xorg-minimal vim xdg-user-dirs zsh picom dex scrot bspwm polybar sxhkd alacritty git vscode firefox Thunar tumbler thunar-archive-plugin file-roller unrar unzip p7zip thunar-media-tags-plugin setxkbmap  nitrogen lxappearance gvfs gvfs-mtp polkit elogind dbus rtkit pavucontrol pipewire alsa-pipewire webp-pixbuf-loader ristretto mousepad mpv mesa-dri mesa-dri-32bit steam numlockx gnome-keyring seahorse nvidia470 nvidia470-libs-32bit 
+#baixar e instalar o Oh My Zsh!
+xdg-user-dirs-update
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# configurar o bspwm
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+#configurar o pipewire
+sudo ln -s /usr/share/applications/pipewire.desktop /etc/xdg/autostart/pipewire.desktop
+sudo ln -s /usr/share/applications/pipewire-pulse.desktop /etc/xdg/autostart/pipewire-pulse.desktop
+sudo mkdir -p /etc/alsa/conf.d
+sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+#ativar serviços runit
+sudo ln -s /etc/sv/dbus/ /var/service/
+sudo ln -s /etc/sv/dhcpcd/ /var/service/
+sudo ln -s /etc/sv/rtkit/ /var/service/
+sudo ln -s /etc/sv/polkitd/ /var/service/
