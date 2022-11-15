f5c5bda1efe34efb32745f893031063d30de1240
passon <114824841+pass0n@users.noreply.github.com>
2022-11-14 23:54:21 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,10 @@ sudo xbps-install -Sy
 #criar a pasta para instalação do nvidia DKMS
 sudo mkdir -p /var/lib/dkms
 #baixar os meus pacotes padrões
-sudo xbps-install -Sy xorg-minimal vim xdg-user-dirs zsh picom dex scrot bspwm polybar sxhkd alacritty git vscode firefox Thunar tumbler thunar-archive-plugin file-roller unrar unzip p7zip thunar-media-tags-plugin setxkbmap  nitrogen lxappearance gvfs gvfs-mtp polkit elogind dbus rtkit pavucontrol pipewire alsa-pipewire webp-pixbuf-loader ristretto mousepad mpv mesa-dri mesa-dri-32bit steam numlockx gnome-keyring seahorse nvidia470 nvidia470-libs-32bit 
+sudo xbps-install -Sy xorg-minimal vim xdg-user-dirs zsh picom dex scrot awesome alacritty git vscode firefox Thunar tumbler thunar-archive-plugin file-roller unrar unzip p7zip thunar-media-tags-plugin setxkbmap  nitrogen lxappearance gvfs gvfs-mtp polkit elogind dbus rtkit pavucontrol pipewire alsa-pipewire ssr webp-pixbuf-loader ristretto geany mpv mesa-dri mesa-dri-32bit steam numlockx gnome-keyring seahorse nvidia470 nvidia470-libs-32bit 
 #baixar e instalar o Oh My Zsh!
 xdg-user-dirs-update
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# configurar o bspwm
-install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
 #configurar o pipewire
 sudo ln -s /usr/share/applications/pipewire.desktop /etc/xdg/autostart/pipewire.desktop
 sudo ln -s /usr/share/applications/pipewire-pulse.desktop /etc/xdg/autostart/pipewire-pulse.desktop
