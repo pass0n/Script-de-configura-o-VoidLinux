e2bbe65b9b2f21d5219a87a41f8c77b21b6c6c7c
pass0n <114824841+pass0n@users.noreply.github.com>
2023-02-12 18:29:27 -0300

    :white_check_mark: update

--- install.sh
+++ install.sh
@@ -1,52 +1,56 @@
 #!/bin/sh
 #script de instalação do meu setup voidlinux.
 
-#entar no modo super usuário
-su root
 #descriptografar e montar os volumes lvm
-cryptsetup luksOpen /dev/sda2 lvm
+cryptsetup luksOpen /dev/sda2 lv
 vgchange -ay
 #formatar as partições
 wipefs -a /dev/sda1
-wipefs -a /dev/lvm/root
-wipefs -a /dev/lvm/swap
+wipefs -a /dev/mapper/lv-root
+wipefs -a /dev/mapper/lv-swap
+
 #criar as partições
 mkfs.vfat -F32 /dev/sda1
-mkfs.ext4 -L VoidLinux /dev/lvm/root
-mkswap /dev/lvm/swap
+mkfs.ext4 -L root /dev/mapper/lv-root
+mkswap /dev/mapper/lv-swap
+
 #montar as partições
-mount /dev/lvm/root /mnt
+mount /dev/mapper/lv-root /mnt
 for dir in dev proc sys run; do mkdir -p /mnt/$dir ; mount --rbind /$dir /mnt/$dir ; mount --make-rslave /mnt/$dir ; done
-mkdir -p /mnt/home
-mount /dev/lvm/home /mnt/home
-mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
+mount --mkdir /dev/mapper/lv-home /mnt/home
+mount --mkdir /dev/sda1 /mnt/boot/efi
+
 #copiar as chaves do xbps
 mkdir -p /mnt/var/db/xbps/keys
 cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
+
 #instalar o sistema base e afins.
-xbps-install -Sy -R https://repo-default.voidlinux.org/current -r /mnt base-system base-devel lvm2 cryptsetup grub-x86_64-efi vim
+xbps-install -Sy -R https://repo-default.voidlinux.org/current -r /mnt base-system base-devel lvm2 cryptsetup grub-x86_64-efi neovim
+
 #remover arquivos e instalar os pré-configurados.
-cd config
-rm /mnt/etc/crypttab
-rm /mnt/etc/fstab
-rm /mnt/etc/hostname
-rm /mnt/etc/locale.conf
-cp cryptab fstab hostname locale.conf /mnt/etc/
-cp 10-key.conf /etc/dracut.conf.d/
-cp grub /etc/default/
-cd /
-#configurações da raiz e root
+
+cp config/10-key.conf /mnt/etc/dracut.conf.d/
+cp config/grub /mnt/etc/default/grub
+cp config/libc-locales /mnt/etc/default/libc-locales
+cp config/locale.conf /mnt/etc/locale.conf
+cp config/fstab /mnt/etc/fstab
+cp config/crypttab /mnt/etc/crypttab
+cp config/hostname /mnt/etc/hostname
+
+#configurações da raiz
 chroot /mnt
 chown root:root /
 chmod 755 /
-cp /home/void.bk/key /
-chmod 000 /key
+cp /home/void.bk/key.bin /
+chmod 000 /key.bin
 passwd root
+
 #instalando o boot
-grub-install --target="x86_64" --efi-directory=:"/boot/efi" --bootloarder-id="VoidLinux" /dev/sda
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloarder-id="void_grub" /dev/sda
+
 #reconfigurar tudo
 xbps-reconfigure -fa
+
 #sair e reiniciar
 exit
 umount -R /mnt
--- script.sh
+++ script.sh
@@ -2,23 +2,30 @@
 #Script de configuração do meu setup VoidLinux
 
 #Baixar e atualizar os repositórios void
-sudo xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
-sudo xbps-install -Sy
+xbps-install -Sy void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+xbps-install -Sy
+
 #criar a pasta para instalação do nvidia DKMS
-sudo mkdir -p /var/lib/dkms
+mkdir -p /var/lib/dkms
+
 #baixar os meus pacotes padrões
-sudo xbps-install -Sy xorg-minimal vim xdg-user-dirs zsh picom dex scrot awesome alacritty git vscode firefox Thunar tumbler thunar-archive-plugin file-roller unrar unzip p7zip thunar-media-tags-plugin setxkbmap  nitrogen lxappearance gvfs gvfs-mtp polkit elogind dbus rtkit pavucontrol pipewire alsa-pipewire ssr webp-pixbuf-loader ristretto geany mpv mesa-dri mesa-dri-32bit steam numlockx gnome-keyring seahorse nvidia470 nvidia470-libs-32bit 
-#baixar e instalar o Oh My Zsh!
-xdg-user-dirs-update
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+xbps-install -Sy xorg-minimal bspwm sxhkd polybar rofi terminator zathura zathura-pdf-mupdf neovim xdg-user-dirs picom dex scrot git vscode eclipse firefox Thunar tumbler thunar-archive-plugin file-roller unrar unzip p7zip thunar-media-tags-plugin setxkbmap xsetroot xprop nitrogen lxappearance gvfs elogind rtkit pavucontrol pipewire alsa-pipewire webp-pixbuf-loader ristretto mpv mesa-dri mesa-dri-32bit numlockx nvidia470 nvidia470-libs-32bit
+
 #configurar o pipewire
-sudo ln -s /usr/share/applications/pipewire.desktop /etc/xdg/autostart/pipewire.desktop
-sudo ln -s /usr/share/applications/pipewire-pulse.desktop /etc/xdg/autostart/pipewire-pulse.desktop
-sudo mkdir -p /etc/alsa/conf.d
-sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
-sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+ln -s /usr/share/applications/pipewire.desktop /etc/xdg/autostart/pipewire.desktop
+ln -s /usr/share/applications/pipewire-pulse.desktop /etc/xdg/autostart/pipewire-pulse.desktop
+mkdir -p /etc/pipewire
+sed '/path.*=.*pipewire-media-session/s/{/#{/' \
+    /usr/share/pipewire/pipewire.conf > /etc/pipewire/pipewire.conf
+mkdir -p /etc/pipewire/pipewire.conf.d
+echo 'context.exec = [ { path = "/usr/bin/wireplumber" args = "" } ]' \
+    > /etc/pipewire/pipewire.conf.d/10-wireplumber.conf
+mkdir -p /etc/alsa/conf.d
+ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+
 #ativar serviços runit
-sudo ln -s /etc/sv/dbus/ /var/service/
-sudo ln -s /etc/sv/dhcpcd/ /var/service/
-sudo ln -s /etc/sv/rtkit/ /var/service/
-sudo ln -s /etc/sv/polkitd/ /var/service/
+ln -s /etc/sv/dbus/ /var/service/
+ln -s /etc/sv/dhcpcd/ /var/service/
+ln -s /etc/sv/rtkit/ /var/service/
+ln -s /etc/sv/polkitd/ /var/service/
